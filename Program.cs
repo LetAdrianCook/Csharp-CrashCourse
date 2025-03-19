a9954e9f5f04cf6f14bfd9b2da70ee7178c0d82e
LetAdrianCook <adrianvillasis2252@gmail.com>
2025-03-19 11:08:24 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Csharp_Writeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");  //Reads from top to bottom, Console.Writeline new line
+            Console.WriteLine("This is my crash course for C#!");
+
+            Console.Write("Same Line"); //Console.Write does not go to the next line
+            Console.Write("Same Line");
+
+            Console.WriteLine("Hello World"); //Combination
+            Console.Write("Hello Programmers");
+            Console.WriteLine("Welcome to C#");
+
+            Console.Write("Hello \nWorld"); //special characters \n = new line
+        }
+    }
+}
